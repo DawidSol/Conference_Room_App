776c685855d78e6a34aee8c50c7c81d80e8b631f
Dawid Sołek <dsolek108@gmail.com>
2024-01-13 10:09:34 +0100

    reservation model

--- Conference_Room_App/main_app/models.py
+++ Conference_Room_App/main_app/models.py
@@ -5,3 +5,13 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     capacity = models.IntegerField()
     projector_available = models.BooleanField()
+
+
+class Reservation(models.Model):
+    date = models.DateField(unique=True)
+    comment = models.TextField(null=True, blank=True)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+class Meta(models.Model):
+    unique_together = ('date', 'room_id')
