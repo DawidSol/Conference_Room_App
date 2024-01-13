9fe1920e4f5c36e0608fbf149b09ca31683f85b8
Dawid Sołek <dsolek108@gmail.com>
2024-01-13 09:36:39 +0100

    room edition

--- Conference_Room_App/main_app/views.py
+++ Conference_Room_App/main_app/views.py
@@ -47,7 +47,39 @@ def room_name(request, room_id):
 
 
 def delete_room(request, room_id):
-    room = Room.objects.get(pk=room_id)
+    try:
+        room = Room.objects.get(pk=room_id)
+    except Room.DoesNotExist:
+        return HttpResponse("Room does not exist!")
     if request.method == 'GET':
         room.delete()
         return redirect('main_app:rooms_list')
+
+
+def modify_room(request, room_id):
+    try:
+        room = Room.objects.get(pk=room_id)
+    except Room.DoesNotExist:
+        return HttpResponse("Room does not exist!")
+    if request.method == 'GET':
+        return render(request, 'modify_room.html',)
+    else:
+        changed_name = request.POST["name"]
+        changed_capacity = request.POST["capacity"]
+        changed_projector = request.POST.get("projector")
+        if not changed_name:
+            return HttpResponse("You must provide a name!")
+        elif Room.objects.filter(name=changed_name).exists():
+            return HttpResponse("Room with this name already exists!")
+        elif not changed_capacity:
+            return HttpResponse("You must provide room capacity!")
+        elif int(changed_capacity) <= 0:
+            return HttpResponse("Capacity must be greater than 0!")
+        elif not changed_projector:
+            return HttpResponse("You must specify if there is a projector or not!")
+        else:
+            room.name = changed_name
+            room.capacity = changed_capacity
+            room.projector = changed_projector
+            room.save()
+            return render(request, "base.html")
